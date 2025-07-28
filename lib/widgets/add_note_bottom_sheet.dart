f28e74ef5d45013ebddf92c1510c3984b6646e44
Osama <osama.87664@gmail.com>
2025-07-28 08:00:47 +0300

    create note button sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -11,12 +12,46 @@ class AddNoteBottomSheet extends StatelessWidget {
         horizontal: 16,
         vertical: 30,
       ),
-      child: Column(
-        children: const [
-          CustomTextField(title: 'Title'),
-          SizedBox(height: 24),
-          CustomTextField(title: 'Content', maxLines: 5),
-        ],
+      child: SingleChildScrollView(
+        // استخدامها للسماح بالتمرير في حالة الحاجة إلى مساحة أكبر من الشاشة
+        child: Column(
+          children: const [
+            CustomTextField(title: 'Title'),
+            SizedBox(height: 24),
+            CustomTextField(title: 'Content', maxLines: 5),
+            SizedBox(height: 32),
+            CustomButton(),
+            SizedBox(height: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(
+        context,
+      ).size.width, // هذا يضمن أن الزر يأخذ عرض الشاشة بالكامل
+      height: 50,
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Center(
+        child: Text(
+          'Add Note',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
