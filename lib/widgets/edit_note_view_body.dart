c848160cfe892c5ce4e3611c48514fc3bf88059a
Osama <osama.87664@gmail.com>
2025-07-28 09:25:05 +0300

    create node model

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_appbar.dart';
-import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
@@ -15,13 +14,10 @@ class EditNoteViewBody extends StatelessWidget {
         children: const [
           SizedBox(height: 50),
           CustomAppBar(title: 'Edit Note', icon: Icons.check),
-          SizedBox(height: 50),
+          SizedBox(height: 60),
           CustomTextField(title: 'Title'),
           SizedBox(height: 24),
           CustomTextField(title: 'Content', maxLines: 5),
-          SizedBox(height: 32),
-          CustomButton(title: 'Add Note'),
-          SizedBox(height: 16),
         ],
       ),
     );
