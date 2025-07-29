4d7245689465c89d96d7bb71ad2e369519dcb832
Osama <osama.87664@gmail.com>
2025-07-29 19:18:30 +0300

    add add note cubit to button sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,77 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_button.dart';
-import 'package:notes_app/widgets/custom_text_field.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/widgets/add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 30),
       child: SingleChildScrollView(
         // استخدامها للسماح بالتمرير في حالة الحاجة إلى مساحة أكبر من الشاشة
         // هذا مفيد عند إضافة ملاحظات طويلة // يمكن أن يكون مفيدًا في حالة وجود لوحة مفاتيح مفتوحة
-        child: AddNoteForm(),
-      ),
-    );
-  }
-}
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailur) {
+              print('Failur ${state.errMessage}');
+            }
 
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({super.key});
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  String? title, subTitle;
-  GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  // AutovalidateMode يحدد متى تظهر أخطاء الفورم تلقائيًا.
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          CustomTextField(
-            title: 'Title',
-            onsaved: (value) {
-              title = value;
-            },
-          ),
-          const SizedBox(height: 24),
-          CustomTextField(
-            title: 'Content',
-            maxLines: 5,
-            onsaved: (value) {
-              subTitle = value;
-            },
-          ),
-          const SizedBox(height: 32),
-          CustomButton(
-            title: 'Add Note',
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                /*🔁 يفحص الأخطاء دائمًا تلقائيًا في الفورم بمجرد ما المستخدم يكتب أو يغير أي شيء، بدون ما ينتظر يضغط زر إرسال.
-                مثال سريع:
-                لو عندك حقل بريد إلكتروني، والمستخدم كتب شيء خطأ، بيطلع له الخطأ على طول وهو يكتب 
-                
-                فيه أوضاع ثانية زي:
-                  AutovalidateMode.disabled: ما يتحقق من الأخطاء إلا لما تضغط زر الإرسال.
-                  AutovalidateMode.onUserInteraction: يبدأ يتحقق بعد أول تفاعل من المستخدم.
-                */
-              }
-            },
-          ),
-          const SizedBox(height: 16),
-        ],
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const AddNoteForm(),
+            );
+          },
+        ),
       ),
     );
   }
